e262154954031ca3da132db46792e29c4954a391
Evren Tükle <tukleevren@gmail.com>
2025-07-04 14:51:22 +0100

    Fixes manin.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,9 @@ app = FastAPI()
 MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
 API_KEY = os.environ.get("API_KEY")
 
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' was not set")
+
 @app.get("/")
 def read_index():
     return {"hello": "world sync again!!!", "project_name": MY_PROJECT}
