c9fe6fb2e297df0ee322726916f73f9c22753250
Evren Tükle <tukleevren@gmail.com>
2025-07-07 19:15:08 +0100

    Updates database models

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,8 +1,9 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from ..db import get_session
-from .models import ChatMessagePayload, ChatMessage
+from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 
 router = APIRouter()
 
@@ -14,7 +15,7 @@ def chat_health():
 
 # /api/chats/recent
 # curl http://localhost:8080/api/chats/recent/
-@router.get("/recent/")
+@router.get("/recent/", response_model=List[ChatMessageListItem])
 def chat_list_messages(session: Session = Depends(get_session)):
     query = select(ChatMessage)
     results = session.exec(query).fetchall()[:10]
@@ -23,7 +24,7 @@ def chat_list_messages(session: Session = Depends(get_session)):
 
 # HTTP POST -> payload = {"message": "Hello World"} -> {"message": "Hello World", "id": 1}
 # curl -X POST -d '{"message": "Hello World"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
-@router.post('/', response_model=ChatMessage)
+@router.post('/', response_model=ChatMessageListItem)
 def chat_create_message(
         payload: ChatMessagePayload,
         session: Session = Depends(get_session)
