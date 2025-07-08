83733eb107a538bdf0b49d26d998ce0a82ead63d
Evren Tükle <tukleevren@gmail.com>
2025-07-08 14:03:17 +0100

    Implements first llm integration.

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from ..db import get_session
+from ..ai.services import generate_email_message
+from ..ai.schemas import EmailMessageSchema
 from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 
 router = APIRouter()
@@ -13,6 +15,7 @@ router = APIRouter()
 def chat_health():
     return {'status': "ok"}
 
+
 # /api/chats/recent
 # curl http://localhost:8080/api/chats/recent/
 @router.get("/recent/", response_model=List[ChatMessageListItem])
@@ -22,9 +25,10 @@ def chat_list_messages(session: Session = Depends(get_session)):
 
     return results
 
+
 # HTTP POST -> payload = {"message": "Hello World"} -> {"message": "Hello World", "id": 1}
-# curl -X POST -d '{"message": "Hello World"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
-@router.post('/', response_model=ChatMessageListItem)
+# curl -X POST -d '{"message": "Give me a summary of why it is good to go outside"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+@router.post('/', response_model=EmailMessageSchema)
 def chat_create_message(
         payload: ChatMessagePayload,
         session: Session = Depends(get_session)
@@ -33,6 +37,8 @@ def chat_create_message(
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    session.refresh(obj)
+    # session.refresh(obj)
 
-    return obj
+    response = generate_email_message(payload.message)
+
+    return response
